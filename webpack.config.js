fac4c953ee15b67be3c25cc020444a2b028a6c70
Tomasz Jakut <vepomoc@gmail.com>
2018-12-03 12:49:38 +0100

    Fix typo in banner comment.

--- webpack.config.js
+++ webpack.config.js
@@ -65,7 +65,7 @@ module.exports = {
 			banner: `/*!*
  * @license Copyright (c) 2003-2018, CKSource - Frederico Knabben. All rights reserved.
  * For licensing, see LICENSE.md.
-*/`,
+ */`,
 			raw: true
 		} ),
 	],
