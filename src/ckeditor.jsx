899df8e879f422822da8dbc3387869473d8c2925
trapajim <mail@tk-dev.de>
2019-02-26 12:37:18 +0900

    updated editor url to newest version

--- src/ckeditor.jsx
+++ src/ckeditor.jsx
@@ -122,6 +122,6 @@ CKEditor.defaultProps = {
 	readOnly: false
 };
 
-CKEditor.editorUrl = 'https://cdn.ckeditor.com/4.11.1/standard-all/ckeditor.js';
+CKEditor.editorUrl = 'https://cdn.ckeditor.com/4.11.2/standard-all/ckeditor.js';
 
 export default CKEditor;
