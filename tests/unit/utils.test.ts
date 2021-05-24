deb5d7c1562db33693b1e18662c74c2f47cf30dd
Michał Malik <m.malik@cksource.com>
2021-05-24 13:01:52 +0200

    test(v2): add test cases

--- tests/unit/utils.test.ts
+++ tests/unit/utils.test.ts
@@ -1,4 +1,4 @@
-import { uniqueName, camelToKebab } from '../../src/utils';
+import { uniqueName, camelToKebab, getRootStyle } from '../../src/utils';
 
 function init() {
 	describe( 'utils', () => {
@@ -17,6 +17,24 @@ function init() {
 			it( 'generates different string values', () => {
 				/* eslint-disable-next-line no-self-compare */
 				expect( uniqueName() === uniqueName() ).toBe( false );
+			} );
+		} );
+
+		describe( 'getRootStyle', () => {
+			it( 'returns default style if editor is not ready', () => {
+				expect( getRootStyle( null, 'loaded' ) ).toEqual( {
+					visibility: 'hidden'
+				} );
+			} );
+
+			it( 'returns passed style if editor is ready', () => {
+				expect( getRootStyle( { borderColor: 'blue' }, 'ready' ) ).toEqual( {
+					borderColor: 'blue'
+				} );
+			} );
+
+			it( 'returns undefined if no style was provided but editor is ready', () => {
+				expect( getRootStyle( null, 'ready' ) ).toBeUndefined();
 			} );
 		} );
 	} );
